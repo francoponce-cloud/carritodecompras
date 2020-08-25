16eafb3f9407fdbd11c97cf3c281a512318c594e
francoponce-cloud <franponce98@gmail.com>
2020-08-25 00:03:39 -0300

    Se agrega logica inicial de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,1 +1,11 @@
-﻿
+﻿class Carrito{
+    // Añadir el producto al carrito
+    comprarProducto(e){
+        e.preventDefault(); // parametro e para prevenir la acción por defecto 
+        if(e.target.classList.contains('agregar-carrito')){ // Si se hace click en el boton comprar de nuestro carrito entonces ese valor lo vamos a guardar. 
+            // Lo que hacemos es almacenar todo ese valor
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+}

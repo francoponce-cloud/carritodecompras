2c8b6750c933d0578275ff1025afe9cddfcc6f50
francoponce-cloud <franponce98@gmail.com>
2020-08-25 00:45:46 -0300

    Se hace logica de agregado productos

--- js/carrito.js
+++ js/carrito.js
@@ -8,4 +8,28 @@
             this.leerDatosProducto(producto);
         }
     }
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h4').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertarCarrito(infoProducto);
+    }
+    insertarCarrito(producto){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+        <img src="${producto.imagen}" width=100>
+        </td>
+        <td>${producto.titulo}</td>
+        <td>${producto.precio}</td>
+        <td>
+        <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+        </td>
+        `;
+        listaPorductos.appendChild(row);
+    }
 }
